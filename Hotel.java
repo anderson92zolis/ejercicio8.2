76db258abb2d4825725a7fb18b1d5d8d541c5422
anderson92zolis <azolis92@hotmail.es>
2022-12-20 15:46:31 +0000

    Creacion de los metodos si existe o no el hotel

---
+++ Hotel.java
@@ -0,0 +1,59 @@
+
+
+public class Hotel(){
+
+  
+    
+    private String nom;
+    private int nombreHabitacions;
+    private int nombrePlantes;
+    private int superfícieTotalHotel;
+
+public Hotel (String nom, int nombreHabitacions, int nombrePlantes, int superfícieTotalHotel){
+    this.nom= nom;
+    this.nombreHabitacions= nombreHabitacions;
+    this.nombrePlantes= nombrePlantes;
+    this.superfícieTotalHotel= superfícieTotalHotel;
+
+}
+// getter
+public String getNom(){
+    return nom;
+}
+
+public int getNombreHabitacions(){
+    return nombreHabitacions;
+}
+
+public int getNombrePlantes(){
+    return nombrePlantes;
+}
+
+public int getSuperficieTotalHotel(){
+    return superfícieTotalHotel;
+}
+//setters
+public void setNom(String nom){
+    this.nom=nom;
+}
+
+public void setNombreHabitacions(int nombreHabitacions){
+    this.nombreHabitacions=nombreHabitacions;
+}
+
+public void setNombrePlantes(int nombrePlantes){
+    this.nombrePlantes=nombrePlantes;
+}
+
+public void setSuperfícieTotalHotel(int superfícieTotalHotel){
+    this.superfícieTotalHotel=superfícieTotalHotel;
+}
+// methods
+public void calcularManteniment(){
+    System.out.println("persones són necessàries per atendre el servei d’habitacions de l’hotel, "+ nom + " son " + (double) nombreHabitacions/20);
+    System.out.println("el cost total destinat al servei: "+ (double) nombreHabitacions/20 *(1500) +" €/mes");
+}
+
+
+
+}
